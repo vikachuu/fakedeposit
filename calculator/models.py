1f979de7dba38557c02272720f1e4b157b495a13
vikachu <kozopasv@gmail.com>
2019-09-22 19:49:12 +0300

    create api

--- calculator/models.py
+++ calculator/models.py
@@ -4,10 +4,18 @@ from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
+from wagtail.api import APIField
+
 
 class CalculatorPage(Page):
     intro = RichTextField(blank=True)
+    # intro = models.CharField(max_length=250)
 
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname="full")
+        # FieldPanel('intro'),
+    ]
+
+    api_fields = [
+        APIField('intro'),
     ]
