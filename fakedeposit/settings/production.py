92987b59b8d206db042afcff7f48cc35a5a81014
vikachu <kozopasv@gmail.com>
2019-10-13 22:15:15 +0300

    test

--- fakedeposit/settings/production.py
+++ fakedeposit/settings/production.py
@@ -11,8 +11,8 @@ DEBUG = False
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # AWS
-MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 COMPRESS_OFFLINE = True
 COMPRESS_CSS_FILTERS = [
