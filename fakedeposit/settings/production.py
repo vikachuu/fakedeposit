db2a9e8212de7bc60488d91cd08f7db24273534b
vikachu <kozopasv@gmail.com>
2019-10-13 23:27:49 +0300

    edit whitenoise version

--- fakedeposit/settings/production.py
+++ fakedeposit/settings/production.py
@@ -8,11 +8,12 @@ SECRET_KEY = env['SECRET_KEY']
 
 DEBUG = False
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # AWS
 MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 COMPRESS_OFFLINE = True
 COMPRESS_CSS_FILTERS = [
