0985baac0bcd64be794706c4a81ed3fe6f52cebe
Viktoriia Kozopas <viktoriiakozopas@Viktoriias-MacBook-Pro.local>
2019-11-16 18:24:24 +0200

    Add page description to home page

--- home/models.py
+++ home/models.py
@@ -4,10 +4,18 @@ from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
+from wagtail.api import APIField
+
 
 class HomePage(Page):
     body = RichTextField(blank=True)
+    page_description = models.CharField(max_length=200, default="string")
 
     content_panels = Page.content_panels + [
         FieldPanel('body', classname="full"),
+        FieldPanel('page_description')
+    ]
+
+    api_fields = [
+        APIField('page_description'),
     ]
